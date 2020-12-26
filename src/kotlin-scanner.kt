65c729ea9d868d0e517cdc7c5cdc91a4acb9a829
niawjunior <niawkung@gmail.com>
2020-12-27 01:15:07 +0700

    Print an integer entered by an user

---
+++ src/kotlin-scanner.kt
@@ -0,0 +1,10 @@
+import java.util.Scanner
+
+fun main() {
+    val reader = Scanner(System.`in`)
+    print("Enter number ")
+
+    val integer: Int = reader.nextInt()
+
+    print("Your entered: $integer")
+}
